03baf5b9336bd29a0d7997e631164e3ee2977c89
Valenciola <valeriez3012@gmail.com>
2025-02-06 21:46:53 -0500

    We can generate matrices!

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//console.log("Hello!");
+genMatrix = function() {
+    const rows = document.getElementById("rows").value;
+    const columns = document.getElementById("columns").value;
+    const table = document.getElementById("matrix");
+    //console.log(rows, columns);
+
+    table.innerHTML = '';
+
+    for (let i = 0; i < rows; i++) {
+        let row = document.createElement("tr");
+        for (let j = 0; j < columns; j++) {
+            entry = document.createElement("td");
+            row.appendChild(entry);
+            entry.id = `c${i + 1}${j + 1}`
+            if (i === j) {
+                entry.innerHTML = "1";
+            }
+            else {
+                entry.innerHTML = "0"
+            }
+        }
+        table.append(row);
+    }
+
+    const tdElements = table.querySelectorAll("td");
+    tdElements.forEach(td => {
+        td.style.width = ((1/columns) * 100 + "%");
+    });
+}
